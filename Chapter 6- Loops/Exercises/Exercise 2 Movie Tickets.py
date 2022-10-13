b919a89cb2bb0369e90b1660cdf5b8f771c56fbd
Railey Inductivo <80341106+RaileyAnthony@users.noreply.github.com>
2022-10-13 21:41:38 +0400

    commit

--- Chapter 6- Loops/Exercises/Exercise 2 Movie Tickets.py
+++ Chapter 6- Loops/Exercises/Exercise 2 Movie Tickets.py
@@ -4,13 +4,11 @@ message = " "
 while True:
     message = input(age)
     if message == 'quit':
-      break
+        break
     if int(message) < 3:
         print("Your ticket is free")
     elif int(message) <= 12:
         print("Your ticket costs 10$")
     elif int(message) > 12:
         print("Your ticket costs 15$")
-
-
 
