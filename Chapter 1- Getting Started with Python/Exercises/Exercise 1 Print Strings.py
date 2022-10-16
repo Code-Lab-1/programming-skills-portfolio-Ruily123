14a884faa0b00daa84e2b8494a5f07d98bc2a415
Railey Inductivo <80341106+RaileyAnthony@users.noreply.github.com>
2022-10-16 16:29:29 +0400

    commit

--- Chapter 1- Getting Started with Python/Exercises/Exercise 1 Print Strings.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise 1 Print Strings.py
@@ -1,3 +1,2 @@
-print(str("Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the "
-          "sky."))
-print(str("Twinkle, twinkle, little star, How I wonder what you are"))
+print(str("Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high,"))
+print(str("Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are"))
