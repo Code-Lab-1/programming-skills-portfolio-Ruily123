f00e999163383efebe4954d57dee9b93e37991be
Railey Inductivo <80341106+RaileyAnthony@users.noreply.github.com>
2022-11-04 18:17:59 +0400

    commit

---
+++ Chapter 7- Functions/Exercises/Exercise 9 Magicians.py
@@ -0,0 +1,8 @@
+def show_magicians(names):
+    for name in names:
+        msg = name
+        print(msg)
+
+magician_names = ['Railey', 'Dyan', 'Rami', 'Nami']
+print("Magician Names: ")
+show_magicians(magician_names)
