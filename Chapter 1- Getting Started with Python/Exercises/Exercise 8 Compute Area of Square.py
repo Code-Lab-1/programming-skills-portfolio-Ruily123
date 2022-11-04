685e93857059862d3d612ed7d41e56c357678643
Railey Inductivo <80341106+RaileyAnthony@users.noreply.github.com>
2022-11-04 10:26:01 +0400

    commit

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise 8 Compute Area of Square.py
@@ -0,0 +1,5 @@
+length = float(input("Input length of square: "))
+
+aos = length**2
+
+print("The area of the square is:", aos)
---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise 9 Adding Two Numbers.py
@@ -0,0 +1,5 @@
+x = float(input("Input first number: "))
+y = float(input("Input second number: "))
+
+sum = x + y
+print("The sum of {0} and {1} is {2}".format(x, y, sum))
